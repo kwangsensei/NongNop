ebb474a6cf3bd67f8543d513e928be69551df1de
Chinapat Rattanapirom <chinapat.r@ku.th>
2023-11-05 21:41:52 +0700

    create get match_table api

---
+++ ioc.js
@@ -0,0 +1,49 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://adminIOC:admin@cluster0.sinzaa9.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = 3000;
+
+const express = require('express');
+const ioc = express ();
+ioc.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+async function run(msg, response) {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const db = client.db('ioc');
+    const collection = db.collection(msg);
+    query = await collection.find().toArray();
+    console.log(query);
+    response.send(query);
+  } 
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+async function clientConnect() {
+  ioc.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+  });
+  ioc.get('/status', (request, response) => {
+    const status = {
+        'Status': 'Running'
+    };    
+    response.send(status);
+    console.log("send status");
+  });
+  ioc.get('/match_table', (request, response) => {run("match_table", response);
+    console.log("send match_table");
+});
+}
+
+clientConnect();
+//run().catch(console.dir);
