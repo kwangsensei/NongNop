e56f80f4bb21e4735a56563bf90d3c9d534d5f43
Chinapat Rattanapirom <chinapat.r@ku.th>
2023-11-06 23:38:59 +0700

    add post api to match_table

--- ioc.js
+++ ioc.js
@@ -2,7 +2,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://adminIOC:admin@cluster0.sinzaa9.mongodb.net/?retryWrites=true&w=majority";
 
-
 const config = require('./config');
 console.log(config);
 
@@ -22,8 +21,24 @@ async function getQuery(msg, request, response) {
     const db = client.db('ioc');
     const collection = db.collection(msg);
     query = await collection.find(request.params).toArray();
-    console.log(request.params);
+    console.log(query);
     response.send(query);
+  } 
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+async function updateQuery(msg, request) {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const db = client.db('ioc');
+    const collection = db.collection(msg);
+    query = await collection.updateOne(request.params, {$set: request.body});
   } 
   finally {
     // Ensures that the client will close when you finish/error
@@ -46,11 +61,15 @@ function clientConnect() {
     getQuery("match_table", request, response);
     console.log("send match_table");
 });
-ioc.get('/match_table/:round', (request, response) => {
-  getQuery("match_table", request, response);
-  console.log("send final match_table");
-});
-
+  ioc.get('/match_table/:round', (request, response) => {
+    getQuery("match_table", request, response);
+    console.log("send final match_table");
+  });
+  ioc.post('/match_table/:sport_id', (request, response) => {
+    console.log(request.body);
+    updateQuery("match_table", request);
+    response.send("complete");
+  });
 }
 
 clientConnect();
