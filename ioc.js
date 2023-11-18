9fc75b4eb73f64913a129714f2af963cb61514ce
Chinapat Rattanapirom <chinapat.r@ku.th>
2023-11-18 18:41:59 +0700

    add restrain for user_statistic post

--- ioc.js
+++ ioc.js
@@ -38,7 +38,31 @@ async function getQuery(msg, request, response) {
 async function updateQuery(msg, request) {
   const db = client.db('ioc');
   const collection = db.collection(msg);
-  query = await collection.updateOne(request.params, {$set: request.body}, {upsert: true});
+  query = await collection.updateOne(request.params, {$set: request.body});
+}
+
+async function insertUserStatistic(msg, request, response) {
+  const db = client.db('ioc');
+  const collection = db.collection(msg);
+  var format = userStatisticFormat(request.body);
+  if(format == true){
+    await collection.updateOne({country: request.body.country}, {$set: request.body}, {upsert: true});
+    response.send("update audience statistic complete.");
+  }
+  else{
+    response.send("Wrong user_statistic format");
+  }
+}
+
+function userStatisticFormat(body){
+  if(Object.keys(body).length != 2){
+    console.log(Object.keys(body).length);
+    return false;
+  }
+  if(body.country == null || body.count == null){
+    return false;
+  }
+  return true;
 }
 
 async function clientConnect() {
@@ -66,11 +90,9 @@ async function clientConnect() {
     try{
       console.log(request.headers);
       console.log(request.body);
-      updateQuery("user_statistic", request);
-      response.send("update audience statistic complete.");
+      insertUserStatistic("user_statistic", request, response);
     }
     catch(error){
-      console.log(error);
       console.log("failed to insert \n", error);
     }
   });
